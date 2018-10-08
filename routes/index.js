bbb00063c53c9d4c3c074f854d9e8846daa4e7bf
Daisho <daishokomiyama@gmail.com>
2018-10-08 18:02:33 -0400

    Adding store page

--- routes/index.js
+++ routes/index.js
@@ -22,4 +22,6 @@ router.post('/add/:wes',
 
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 
+router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
+
 module.exports = router;
