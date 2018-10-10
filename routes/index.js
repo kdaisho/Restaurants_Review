f3e25cac84be215762065fda53a2064761989bba
Daisho <daishokomiyama@gmail.com>
2018-10-09 22:30:56 -0400

    Adding registration form

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
@@ -25,5 +26,13 @@ router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
 router.get('/tags', catchErrors(storeController.getStoreByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoreByTag));
+
+router.get('/login', userController.loginForm);
+router.get('/register', userController.registerForm);
+
+// 1. Validate the registration data
+// 2. Register the user
+// 3. We need to log them in
+router.post('/register', userController.validateRegister);
 
 module.exports = router;
