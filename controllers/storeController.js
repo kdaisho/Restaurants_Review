9d7048516d731a3cb84b4e5673343cc2018152f7
Daisho <daishokomiyama@gmail.com>
2018-10-19 19:58:24 -0400

    Adding heart stores view

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -151,3 +151,12 @@ exports.heartStore = async (req, res) => {
         );
     res.json(user);
 };
+
+exports.getHearts = async (req, res) => {
+    const stores = await Store.find({
+        _id: {
+            $in: req.user.hearts
+        }
+    });
+    res.render('stores', { title: 'Hearted Stores', stores: stores });
+};
